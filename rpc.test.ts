e5ceda0cdb292e87e7fe98e5c03e172ad302ac87
Mo Prosper <mothepro@live.com>
2025-10-13 19:25:51 -0500

    improve comments

--- rpc.test.ts
+++ rpc.test.ts
@@ -2,10 +2,43 @@ import { expect, test } from 'bun:test'
 import payloads from './payloads.json'
 import verify from './rpc'
 
-let count = 0
-for (const payload of payloads)
-  test(`rpc valid ${++count}`, () => expect(verify(payload)).resolves.toBe(true))
+// todo make this match the verify tests exactly...
 
-count = 0
-for (const payload of payloads)
-  test(`rpc invalid ${++count}`, () => expect(verify(payload)).resolves.toBe(false))
+test(`Test payloads available`, () => expect(payloads.length).not.toBeArrayOfSize(0))
+
+for (const { address, message, signature } of payloads) {
+  test(`\n${message}\nthe above message was signed by ${address}`, () =>
+    expect(verify({ address, signature, message })).resolves.toBe(true))
+
+  // TODO add more!!
+  // Any mutation should always fail
+  const tamperAttempts = new Map([
+    [
+      `${address} did not sign with whitespace padding`,
+      { address, signature, message: ` ${message}` },
+    ],
+    [
+      `${address} did not sign with extra characters`,
+      { address, signature, message: `${message}.` },
+    ],
+    [
+      `${address} did not sign with missing prefix`,
+      { address, signature, message: message.substring(1) },
+    ],
+    [
+      `${address} did not sign with missing suffix`,
+      { address, signature, message: message.substring(0, message.length - 1) },
+    ],
+    [
+      `${address} did not sign a substring`,
+      {
+        address,
+        signature,
+        message: message.substring(1 + Math.floor(Math.random() * message.length)),
+      },
+    ],
+  ])
+
+  for (const [title, payload] of tamperAttempts)
+    test(title, () => expect(verify(payload)).rejects.toBeTruthy())
+}
